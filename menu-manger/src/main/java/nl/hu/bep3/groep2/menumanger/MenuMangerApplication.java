5b758128172cf5052cfd642628004ba094d53bd1
Gerson <gerson@makweb.nl>
2021-11-26 10:55:37 +0100

    Initialisation

---
+++ menu-manger/src/main/java/nl/hu/bep3/groep2/menumanger/MenuMangerApplication.java
@@ -0,0 +1,13 @@
+package nl.hu.bep3.groep2.menumanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuMangerApplication.class, args);
+	}
+
+}
---
+++ menu-manger/src/test/java/nl/hu/bep3/groep2/menumanger/MenuMangerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hu.bep3.groep2.menumanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuMangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
