78461a24f8370c72641893c7312aac4749c16403
Michel <m.kwantes@hotmail.com>
2021-12-02 12:12:00 +0100

    First setup

---
+++ overviewmanger/src/test/java/nl/hu/bep3/groep2/overviewmanger/OverviewmangerApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.hu.bep3.groep2.overviewmanger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverviewmangerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
