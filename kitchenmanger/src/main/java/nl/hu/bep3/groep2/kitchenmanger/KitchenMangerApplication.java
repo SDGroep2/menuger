08d978cad7e064f57e2506e5dea7bd1cef99542b
joost56 <60775638+joost56@users.noreply.github.com>
2021-12-02 12:16:07 +0100

    added kitchenmanger

---
+++ kitchenmanger/src/main/java/nl/hu/bep3/groep2/kitchenmanger/KitchenMangerApplication.java
@@ -0,0 +1,13 @@
+package nl.hu.bep3.groep2.kitchenmanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KitchenMangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KitchenMangerApplication.class, args);
+	}
+
+}
