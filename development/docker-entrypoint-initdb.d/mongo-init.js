2800f90ee01029fbedaed9a54a8e886f93d2a990
tygos <tygo.steenbergen@student.hu.nl>
2021-12-15 13:59:24 +0100

    Database configuration fix

---
+++ development/docker-entrypoint-initdb.d/mongo-init.js
@@ -0,0 +1,42 @@
+print('Start #################################################################');
+
+db = db.getSiblingDB('inventorymanger');
+db.createUser(
+    {
+        user: 'steve',
+        pwd: 'password',
+        roles: [{ role: 'readWrite', db: 'inventorymanger' }],
+    },
+);
+db.createCollection('users');
+
+db = db.getSiblingDB('menumanger');
+db.createUser(
+    {
+        user: 'steve',
+        pwd: 'password',
+        roles: [{ role: 'readWrite', db: 'menumanger' }],
+    },
+);
+db.createCollection('users');
+
+db = db.getSiblingDB('kitchenmanger');
+db.createUser(
+    {
+        user: 'steve',
+        pwd: 'password',
+        roles: [{ role: 'readWrite', db: 'kitchenmanger' }],
+    },
+);
+db.createCollection('users');
+db = db.getSiblingDB('overviewmanger');
+db.createUser(
+    {
+        user: 'steve',
+        pwd: 'password',
+        roles: [{ role: 'readWrite', db: 'overviewmanger' }],
+    },
+);
+db.createCollection('users');
+
+print('END #################################################################');
