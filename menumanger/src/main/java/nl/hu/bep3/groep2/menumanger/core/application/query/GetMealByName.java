beb512c8c1dd63d3a7fa48122eb9e76aebd0b908
Gerson <44945972+ItsGers8@users.noreply.github.com>
2021-12-16 20:26:39 +0100

    Updated order saving

---
+++ menumanger/src/main/java/nl/hu/bep3/groep2/menumanger/core/application/query/GetMealByName.java
@@ -0,0 +1,4 @@
+package nl.hu.bep3.groep2.menumanger.core.application.query;
+
+public record GetMealByName(String name) {
+}
--- menumanger/src/main/java/nl/hu/bep3/groep2/menumanger/core/application/query/GetMenusByName.java
+++
@@ -1,7 +0,0 @@
-package nl.hu.bep3.groep2.menumanger.core.application.query;
-
-import lombok.Getter;
-
-@Getter
-public record GetMenusByName(String name) {
-}
