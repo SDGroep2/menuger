5973dc2996a067a0e66f267e80cdf6166ad2e007
joost56 <60775638+joost56@users.noreply.github.com>
2021-12-16 16:49:17 +0100

    added update order, create order and findAllOrders

--- kitchenmanger/src/main/java/nl/hu/bep3/groep2/kitchenmanger/infrastructure/driver/web/request/OrderRequest.java
+++
@@ -1,8 +0,0 @@
-package nl.hu.bep3.groep2.kitchenmanger.infrastructure.driver.web.request;
-
-import javax.validation.constraints.NotBlank;
-
-public class OrderRequest {
-    @NotBlank
-    public String order;
-}
