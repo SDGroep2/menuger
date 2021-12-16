82b80d716d5ae7176bea62bd6c37d5bcb12e95be
Gerson <44945972+ItsGers8@users.noreply.github.com>
2021-12-16 20:26:39 +0100

    Updated order saving

--- menumanger/src/main/java/nl/hu/bep3/groep2/menumanger/core/application/MealCommandHandler.java
+++ menumanger/src/main/java/nl/hu/bep3/groep2/menumanger/core/application/MealCommandHandler.java
@@ -2,14 +2,13 @@ package nl.hu.bep3.groep2.menumanger.core.application;
 
 import com.mongodb.DuplicateKeyException;
 import nl.hu.bep3.groep2.menumanger.core.application.command.CreateMeal;
+import nl.hu.bep3.groep2.menumanger.core.application.command.CreateOrder;
 import nl.hu.bep3.groep2.menumanger.core.domain.Meal;
 import nl.hu.bep3.groep2.menumanger.core.domain.exception.MealAlreadyExistsException;
 import nl.hu.bep3.groep2.menumanger.core.port.storage.MealRepository;
 import nl.hu.bep3.groep2.menumanger.core.domain.event.MealWasCreated;
 import nl.hu.bep3.groep2.menumanger.core.port.messaging.Publisher;
 import org.springframework.stereotype.Service;
-
-import java.util.UUID;
 
 @Service
 public class MealCommandHandler {
@@ -22,14 +21,17 @@ public class MealCommandHandler {
 	}
 
 	public Meal handle(CreateMeal newMenu) {
-		Meal meal = new Meal(newMenu.getName(),
-				newMenu.getPrice(), newMenu.getIngredients());
-		try {
-			repository.save(meal);
-		} catch (DuplicateKeyException e) {
+		Meal meal = new Meal(newMenu.name(),
+				newMenu.price(), newMenu.ingredients());
+		if (repository.findByName(newMenu.name()).isPresent()) {
 			throw new MealAlreadyExistsException("Meal with name " + meal.getName() + " already exists");
 		}
+		repository.save(meal);
 		publisher.publish(new MealWasCreated(meal));
 		return meal;
+	}
+
+	public CreateOrder handle(CreateOrder createOrder) {
+		return createOrder;
 	}
 }
