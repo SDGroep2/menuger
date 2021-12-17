f67c02a0f5f2ce2432bf98a3b7c52167114766dc
Gerson <gerson@makweb.nl>
2021-12-17 12:02:24 +0100

    Fixed communication errors

--- menumanger/src/main/java/nl/hu/bep3/groep2/menumanger/core/application/MealCommandHandler.java
+++ menumanger/src/main/java/nl/hu/bep3/groep2/menumanger/core/application/MealCommandHandler.java
@@ -1,19 +1,15 @@
 package nl.hu.bep3.groep2.menumanger.core.application;
 
-import com.mongodb.DuplicateKeyException;
 import nl.hu.bep3.groep2.menumanger.core.application.command.CreateMeal;
 import nl.hu.bep3.groep2.menumanger.core.application.command.CreateOrder;
 import nl.hu.bep3.groep2.menumanger.core.domain.Meal;
+import nl.hu.bep3.groep2.menumanger.core.domain.event.MealWasCreated;
 import nl.hu.bep3.groep2.menumanger.core.domain.exception.MealAlreadyExistsException;
 import nl.hu.bep3.groep2.menumanger.core.domain.exception.MealNotFoundException;
+import nl.hu.bep3.groep2.menumanger.core.port.messaging.Publisher;
 import nl.hu.bep3.groep2.menumanger.core.port.storage.KitchenRepository;
 import nl.hu.bep3.groep2.menumanger.core.port.storage.MealRepository;
-import nl.hu.bep3.groep2.menumanger.core.domain.event.MealWasCreated;
-import nl.hu.bep3.groep2.menumanger.core.port.messaging.Publisher;
 import org.springframework.stereotype.Service;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class MealCommandHandler {
