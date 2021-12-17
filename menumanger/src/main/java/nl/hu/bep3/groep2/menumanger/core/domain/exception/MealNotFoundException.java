6cf40eb19177cfcbd8170949aa961d44fbd135b2
Gerson <gerson@makweb.nl>
2021-12-17 11:01:07 +0100

    Added httprepositories

--- menumanger/src/main/java/nl/hu/bep3/groep2/menumanger/core/domain/exception/MealNotFoundException.java
+++ menumanger/src/main/java/nl/hu/bep3/groep2/menumanger/core/domain/exception/MealNotFoundException.java
@@ -2,6 +2,6 @@ package nl.hu.bep3.groep2.menumanger.core.domain.exception;
 
 public class MealNotFoundException extends RuntimeException {
 	public MealNotFoundException(String message) {
-		super(message);
+		super("No meal with the name " + message + " was found!");
 	}
 }
