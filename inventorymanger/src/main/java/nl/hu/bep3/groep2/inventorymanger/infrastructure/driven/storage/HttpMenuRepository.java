5d39e3e8aa2df6f637fbfc9eec62c9bbe098363b
tygos <tygo.steenbergen@student.hu.nl>
2021-12-22 16:08:07 +0100

    zooi

---
+++ inventorymanger/src/main/java/nl/hu/bep3/groep2/inventorymanger/infrastructure/driven/storage/HttpMenuRepository.java
@@ -0,0 +1,23 @@
+package nl.hu.bep3.groep2.inventorymanger.infrastructure.driven.storage;
+
+import nl.hu.bep3.groep2.inventorymanger.core.ports.storage.MenuRepository;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.Map;
+
+public class HttpMenuRepository implements MenuRepository {
+    private final String rootPath;
+    private final RestTemplate client;
+
+    public HttpMenuRepository(String rootPath, RestTemplate client) {
+        this.rootPath = rootPath;
+        this.client = client;
+    }
+
+    @Override
+    public Map<String, Integer> getIngredientsOfMeal(String meal) {
+        URI uri = URI.create(this.rootPath + "/" + meal + "/ingredients");
+        return (Map<String, Integer>) this.client.getForObject(uri, Map.class);
+    }
+}
--- inventorymanger/src/main/java/nl/hu/bep3/groep2/inventorymanger/infrastructure/driver/MessageListener.java
+++ inventorymanger/src/main/java/nl/hu/bep3/groep2/inventorymanger/infrastructure/driver/MessageListener.java
@@ -4,6 +4,7 @@ import nl.hu.bep3.groep2.inventorymanger.core.application.InventoryCommandHandle
 import nl.hu.bep3.groep2.inventorymanger.core.application.command.OrderCreated;
 import nl.hu.bep3.groep2.inventorymanger.core.application.command.OrderUpdated;
 import nl.hu.bep3.groep2.inventorymanger.core.application.exceptions.IngredientNotFoundException;
+import nl.hu.bep3.groep2.inventorymanger.core.application.exceptions.NotEnoughIngredientsException;
 import nl.hu.bep3.groep2.inventorymanger.infrastructure.driver.messaging.event.OrderPlacedEvent;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
@@ -17,13 +18,12 @@ public class MessageListener {
     }
 
     @RabbitListener(queues = "#{'${kitchen-inventory.queue}'}")
-    public void listen(OrderPlacedEvent event) throws IngredientNotFoundException {
+    public void listen(OrderPlacedEvent event) throws IngredientNotFoundException, NotEnoughIngredientsException {
         switch (event.getEventKey()) {
-            //TODO: Stop passing ingredients here and get them from kitchen database in handler
-            case "kitchen.orders.created" -> inventoryCommandHandler.handle(new OrderCreated(event.getIngredients()));
+            case "kitchen.orders.created" -> inventoryCommandHandler.handle(new OrderCreated(event.getMeals()));
             case "kitchen.orders.updated" -> inventoryCommandHandler.handle(new OrderUpdated(event.getOrderId(),
                     event.getStatus(),
-                    event.getIngredients()));
+                    event.getMeals()));
         }
     }
 }
