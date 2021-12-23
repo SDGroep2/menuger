78aab7d69646e2258f7de4bc4e0a64b91f1f5b84
Gerson <44945972+ItsGers8@users.noreply.github.com>
2021-12-23 11:23:07 +0100

    Updated CreateOrder to match Kitchen expectations

--- menumanger/src/main/java/nl/hu/bep3/groep2/menumanger/core/application/command/CreateOrder.java
+++ menumanger/src/main/java/nl/hu/bep3/groep2/menumanger/core/application/command/CreateOrder.java
@@ -3,8 +3,9 @@ package nl.hu.bep3.groep2.menumanger.core.application.command;
 import com.fasterxml.jackson.annotation.JsonCreator;
 
 import java.util.List;
+import java.util.Map;
 
-public record CreateOrder(int table, List<String> meals) {
+public record CreateOrder(int table, Map<String, Integer> meals) {
 	@JsonCreator
 	public CreateOrder {
 
